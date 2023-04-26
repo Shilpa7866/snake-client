54065edd712b18e011555db9c57e621ec2cd4e49
Shilpa Pulipati <shilpa.pulipati24@gmail.com>
2023-04-26 03:06:28 +0000

    adding updated with snake name client.js file

--- client.js
+++ client.js
@@ -1,9 +1,22 @@
 const net = require("net");
 const connect = function () {
 const conn = net.createConnection({
-    host: "localhost", // IP address here,
+    host: "10.0.2.15", // IP address here,
     port: 50541,// PORT number here,
   });
+ conn.setEncoding("utf8");
+conn.on("connect", () => {
+    console.log("connect");
+    conn.write("Name: SHI");
+ console.log("Name");
+    //setInterval( func = () => { conn.write("Move: up");  }, 500);
+  });
+
+  conn.on("data", (data) => {
+    console.log("Server says: ", data);
+  });
+
+  return conn;
 };
 
 module.exports = connect;
