b93e81d11d964b4516ced460db9e9d0025b27f56
Shilpa Pulipati <shilpa.pulipati24@gmail.com>
2023-04-26 03:07:22 +0000

    adding updated with snake name play.js file

--- play.js
+++ play.js
@@ -1,21 +1,7 @@
 const net = require("net");
-const conn = net.createConnection({
-    host: "localhost", // IP address here,
-    port: 50541,// PORT number here,
-  });
-
-
 const connect  = require("./client");
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
-  conn.on("connect", () => {
-   console.log("connected"); // code that does something when the connection is first established
-  });
-  conn.on("data", (data) =>  {
-    console.log("server says,", data); // code that does something when the connection is first established
-  }); 
-  return conn;
-
 console.log("Connecting ...");
+
+
 connect();
 
