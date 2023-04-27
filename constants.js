96d4904c21148411b50e7dfca2ba5ea09159aaa9
Shilpa Pulipati <shilpa.pulipati24@gmail.com>
2023-04-27 23:42:41 +0000

    adding constants.js file

---
+++ constants.js
@@ -0,0 +1,17 @@
+const IP = '10.0.2.15';
+const PORT = 50541;
+
+const UPKEY = "Move: up";
+const LEFTKEY = "Move: left";
+const DOWNKEY = "Move: down";
+const RIGHTKEY = "Move: right";
+
+module.exports = {
+  IP,
+  PORT,
+  UPKEY,
+  LEFTKEY,
+  DOWNKEY,
+  RIGHTKEY
+};
+
